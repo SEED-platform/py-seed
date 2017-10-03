0e4aea0cc8858f925e47816a189142d6470ab398
Paul Munday <paul@paulmunday.net>
2017-10-03 12:18:53 -0700

    initial commit

---
+++ pyseed/apibase.py
@@ -0,0 +1,283 @@
+#!/usr/bin/env python
+"""
+copyright (c) 2016 Earth Advantage. All rights reserved.
+..codeauthor::Paul Munday <paul@paulmunday.net>
+
+Functionality for calls to external API's"""
+
+# Imports from Standard Library
+import re
+
+# Imports from External Modules
+import requests
+
+
+# Public Functions and Classes
+# Helper functions for use by BaseAPI subclasses
+def add_pk(url, pk, required=True, slash=False):
+    """Add id/primary key to url"""
+    if required and not pk:
+        raise APIClientError('id/pk must be supplied')
+    if pk:
+        if isinstance(pk, str) and not pk.isdigit():
+            raise TypeError('id/pk must be a positive integer')
+        elif not isinstance(pk, (int, str)) or int(pk) < 0:
+            raise TypeError('id/pk must be a positive integer')
+        if not url.endswith('/'):
+            url = "{}/{}".format(url, pk)
+        else:
+            url = "{}{}".format(url, pk)
+    if slash:
+        url = "{}/".format(url)
+    return url
+
+
+class APIClientError(Exception):
+    pass
+
+
+class BaseAPI(object):
+    """
+    Base class for API Calls
+    """
+    # pylint: disable=too-few-public-methods, too-many-instance-attributes
+
+    def __init__(self, url=None, use_ssl=True, timeout=None, use_json=False,
+                 use_auth=False, auth=None, **kwargs):
+        # pylint: disable=too-many-arguments
+        """Set url,api key, auth usage, ssl usage, timeout etc.
+
+        :param url: url to use, http(s)://can be omitted, an error will
+                   will be used if it is supplied and dose not match use_ssl
+        :param: use_ssl: connect over https, defaults to true
+        :param use_auth: use authentication
+
+        ..Note:
+            if use_auth is True the default is to use http basic
+            authentication if self.auth is not set. (You will need to
+            to this by overidding __init__ and setting this before
+            calling super.
+
+            This requires username and password to be supplied as
+            keyword arguments. N.B. api keys using basic auth e.g. SEED
+            should be supplied as password.
+
+            To use Digest Authentication set auth_method='digest'
+
+            If use_ssl is False and the url you supply starts with https
+            an error will be thrown.
+        """
+        self.timeout = timeout
+        self.use_ssl = use_ssl
+        self.use_json = use_json
+        self.use_auth = use_auth
+        self.auth = auth
+        self.url = None
+        self.url = self._construct_url(url) if url else None
+        for key, val in kwargs.items():
+            setattr(self, key, val)
+
+    def _construct_payload(self, params):
+        """Construct parameters for an api call.
+.
+        :param params: An dictionary of key-value pairs to include
+            in the request.
+        :return: A dictionary of k-v pairs to send to the server
+            in the request.
+        """
+        compulsory = getattr(self, 'compulsory_params', [])
+        for param in compulsory:
+            if param not in params:
+                try:
+                    params[param] = getattr(self, param)
+                except AttributeError:
+                    msg = "{} is a compulsory field".format(param)
+                    raise APIClientError(msg)
+        if self.auth:                                        # pragma: no cover
+            params['auth'] = self.auth
+        return params
+
+    def _construct_url(self, urlstring, use_ssl=None):
+        """Construct url"""
+        # self.use_ssl takes priority to enforce ssl use
+        use_ssl = self.use_ssl if self.use_ssl is not None else use_ssl
+        if not urlstring and not self.url:
+            raise APIClientError('No url set')
+        elif not urlstring:
+            url = self.url
+        else:
+            if urlstring.startswith('https://') and not use_ssl:
+                # We strip off url prefix
+                # raise an error if https is used  in url without use_ssl
+                raise APIClientError(
+                    'use_ssl is false but url starts with https'
+                )
+            elif urlstring.startswith('http://') and use_ssl:
+                # We strip off url prefix
+                # raise an error if http is used in url with use_ssl
+                raise APIClientError(
+                    'use_ssl is true but url does not starts with https'
+                )
+            else:
+                # strip http(s):// off url
+                regex = re.compile('^https?://')
+                urlstring = regex.sub('', urlstring)
+                if use_ssl:
+                    start = 'https://'
+                else:
+                    start = 'http://'
+                url = "{}{}".format(start, urlstring)
+        return url
+
+    def check_call_success(self, response):
+        """Return true if api call was successfull."""
+        # pylint: disable=no-self-use, no-member
+        return response.status_code == requests.codes.ok
+
+    def _get(self, url=None, use_ssl=None, **kwargs):
+        """Internal method to make api calls using GET."""
+        url = self._construct_url(url, use_ssl=use_ssl)
+        params = self._construct_payload(kwargs)
+        payload = {
+            'timeout': self.timeout,
+            'headers': params.get('headers', None)
+        }
+        if params:
+            payload['params'] = params
+        if self.auth:                                       # pragma: no cover
+            payload['auth'] = self.auth
+        api_call = requests.get(url, **payload)
+        return api_call
+
+    def _post(self, url=None, use_ssl=None, params=None, files=None, **kwargs):
+        """Internal method to make api calls using POST."""
+        url = self._construct_url(url, use_ssl=use_ssl)
+        if not params:
+            params = {}
+        params = self._construct_payload(params)
+        payload = {
+            'timeout': self.timeout,
+            'headers': params.get('headers', None)
+        }
+        if params:
+            payload['params'] = params
+        if files:
+            payload['files'] = files
+        if self.auth:                                       # pragma: no cover
+            payload['auth'] = self.auth
+        if self.use_json:
+            payload['json'] = kwargs
+        else:
+            payload['data'] = kwargs
+        api_call = requests.post(url, **payload)
+        return api_call
+
+    def _put(self, url=None, use_ssl=None, params=None, files=None,
+             **kwargs):
+        """Internal method to make api calls using PUT."""
+        url = self._construct_url(url, use_ssl=use_ssl)
+        if not params:
+            params = {}
+        params = self._construct_payload(params)
+        payload = {
+            'timeout': self.timeout,
+            'headers': params.get('headers', None)
+        }
+        if params:
+            payload['params'] = params
+        if files:                                           # pragma: no cover
+            payload['files'] = files
+        if self.auth:                                       # pragma: no cover
+            payload['auth'] = self.auth
+        if self.use_json:
+            payload['json'] = kwargs
+        else:
+            payload['data'] = kwargs
+        api_call = requests.put(url, **payload)
+        return api_call
+
+    def _patch(self, url=None, use_ssl=None, params=None, files=None,
+               **kwargs):
+        """Internal method to make api calls using PATCH."""
+        url = self._construct_url(url, use_ssl=use_ssl)
+        if not params:
+            params = {}
+        params = self._construct_payload(params)
+        payload = {
+            'timeout': self.timeout,
+            'headers': params.get('headers', None)
+        }
+        if params:
+            payload['params'] = params
+        if files:
+            payload['files'] = files
+        if self.auth:                                        # pragma: no cover
+            payload['auth'] = self.auth
+        if self.use_json:
+            payload['json'] = kwargs
+        else:
+            payload['data'] = kwargs
+        api_call = requests.patch(url, **payload)
+        return api_call
+
+    def _delete(self, url=None, use_ssl=None, **kwargs):
+        """Internal method to make api calls using DELETE."""
+        url = self._construct_url(url, use_ssl=use_ssl)
+        params = self._construct_payload(kwargs)
+        payload = {
+            'timeout': self.timeout,
+            'headers': params.get('headers', None)
+        }
+        if params:
+            payload['params'] = params
+        if self.auth:                                        # pragma: no cover
+            payload['auth'] = self.auth
+        api_call = requests.delete(url, **payload)
+        return api_call
+
+
+class JSONAPI(BaseAPI):
+    """
+    Base class for Json API Calls. See BaseAPI for documentation.
+    """
+    # pylint: disable=too-few-public-methods, too-many-arguments
+
+    def __init__(self, url=None, use_ssl=True, timeout=None,
+                 use_auth=False, auth=None, **kwargs):
+        super(JSONAPI, self).__init__(
+            url=url, use_ssl=use_ssl, timeout=timeout, use_json=True,
+            use_auth=use_auth, auth=auth, **kwargs
+        )
+
+
+class UserAuthMixin(object):
+    """
+    Mixin to provide basic or digest api client authentication via username
+    and password(or api_key)."""
+    # pylint:disable=too-few-public-methods
+
+    def _get_auth(self):
+        """Get basic or digest auth by username/password"""
+        username = getattr(self, 'username', None)
+        password = getattr(self, 'password', None)
+        # support using api_key as password in basic auth
+        # as used by SEED (if supplied as api_key not password)
+        if not password:
+            password = getattr(self, 'api_key', None)
+        if getattr(self, 'auth_method', None) == 'digest':
+            auth = requests.auth.HTTPDigestAuth(username, password)
+        else:
+            auth = requests.auth.HTTPBasicAuth(username, password)
+        return auth
+
+    def _construct_payload(self, params):
+        """Construct parameters for an api call.
+.
+        :param params: An dictionary of key-value pairs to include
+            in the request.
+        :return: A dictionary of k-v pairs to send to the server
+            in the request.
+        """
+        if getattr(self, 'use_auth', None) and not getattr(self, 'auth', None):
+            self.auth = self._get_auth()
+        return super(UserAuthMixin, self)._construct_payload(params)
---
+++ pyseed/seedclient.py
@@ -0,0 +1,414 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+copyright (c) 2016 Earth Advantage. All rights reserved.
+..codeauthor::Paul Munday <paul@paulmunday.net>
+
+Functionality for interacting with the SEED API.
+
+This should be considered a private API.
+
+Use the classes and functions from gbr/common/data/seedrecords.py and
+gbr/common/api.py for general use.
+
+N.B. Only a Read Only client (with public methods) is supplied.
+This is a deliberate design decision. There is no general purpose client that
+can write to the db, this ensures caching is transparent and always valid.
+
+You *must* always use the class corresponsing to the relevant model, i.e.
+one that inherits from SEEDRecord to be able to write to the db.
+You *should* generally this for reading too, in order to get the benefits of
+caching.
+
+"""
+
+# Imports from Standard Library
+import inspect
+
+# Imports from Third Party Modules
+import requests
+
+# Imports from Django
+
+# Local Imports
+from pyseed.apibase import (add_pk, JSONAPI, UserAuthMixin, APIClientError)
+
+
+# Constants
+URLS = {
+    'columns': '/api/v2/columns/',
+    'column_mappings': '/api/v2/column_mappings/',
+    'cycles': '/api/v2/cycles/',
+    'datasets': '/api/v2/datasets/',
+    'gbr_properties': '/api/v2/gbr_properties/',
+    'green_assessment': '/api/v2/green_assessments/',
+    'green_assessment_property': '/api/v2/green_assessment_properties/',
+    'green_assessment_url': '/api/v2/green_assessment_urls/',
+    'labels': '/api/v2/labels/',
+    'import_files': '/api/v2/import_files/',
+    'projects': '/api/v2/projects/',
+    'properties': '/api/v2/properties/',
+    'property_states': '/api/v2/property_states/',
+    'property_views': '/api/v2/property_views/',
+    'taxlots': '/api/v2/taxlots/',
+    'users': '/api/v2/users/',
+}
+
+
+# Private Classes and Functions
+def _get_urls(base_url, url_map=None):
+    """Populate URL"""
+    if not url_map:
+        url_map = URLS
+    return {
+        key: '{}/{}'.format(base_url.rstrip('/'), val.lstrip('/'))
+        for key, val in url_map.items()
+    }
+
+
+def _set_default(obj, key, val, required=True):
+    """
+    Sets val to obj.key if val is None and obj.key is set.
+    Raises Attribute error is neither are set and required is True.
+
+    :returns: val
+    """
+    if not val:
+        val = getattr(obj, key, None)
+    if not val and required:
+        msg = '{} is not set'.format(key)
+        raise AttributeError(msg)
+    return val
+
+
+# Public Classes and Functions
+class SEEDError(APIClientError):
+    # pylint:disable=too-few-public-methods
+    pass
+
+
+class SEEDBaseClient(UserAuthMixin, JSONAPI):
+    """Interact with SEED API.
+
+    Raises a SEEDError on an API Error. No further logging or error
+    handling is done. This the responsibility of the caller.
+
+    This should never be used directly, instead inherit from
+    SEEDReadOnlyClient or SEEDRecord.
+
+    Note subclasses of these should not themselves be inherited from due
+    to the way error handling works, this should not be needed, other classes
+    can inherit from them directly and overwrite methods/use mixins as
+    appropriate.
+
+    endpoint referers to the endpoint name. This allow you to call an
+    endpoint without having to know the full url.
+
+    Endpoint names are set in config, and can be accessed as self.endpoints.
+
+    data_name is set as an attribute on the view called.
+    This constains the actual response data.
+    If not set it is derived from the url (typically its the view name).
+    In either case 'data' is used as a fallback, then detail.
+
+    This is an annoyance, but SEED adds an unnecessary 'status'
+    to its return values to indicate success/failure rather than
+    using status codes (though some endpoints also do this) and returning
+    the result directly.
+
+    :param org_id: organization id of org owning records
+    :type org_id: int
+    :param username: username (email) of user who can access records
+    :type username: string (email address)
+    :param api_key: api_key of use who can access records
+    :type api_key: string
+    :param endpoint: seed endpoint e.g properties for /api/v2/properties/
+    :type endpoint: string
+    :param data_name: name of json key in api results containing data
+                      not always needed
+    :type data_name: string (result are json keys: 'status', data_name)
+    :param config: config object
+    :type config: gbr.common.config.GBRConfig
+    :param config_urls_key: key for urls in config object (default urls)
+    :type config_urls_key: str
+    """
+    # pylint:disable=too-few-public-methods,too-many-arguments
+    # pylint:disable=too-many-instance-attributes
+    def __init__(self, org_id, username=None, password=None, access_token=None,
+                 endpoint=None, data_name=None, use_ssl=None,
+                 base_url=None, port=None, url_map=None, **kwargs):
+        use_ssl = use_ssl if use_ssl else True
+        super(SEEDBaseClient, self).__init__(
+            username=username, password=password, use_ssl=use_ssl,
+            use_auth=True, access_token=access_token, **kwargs
+        )
+        self.org_id = org_id
+        self.token = access_token
+        if not url_map:
+            url_map = URLS
+        # prevent overriding if set in sublcass as class attr
+        if not getattr(self, 'endpoint', None):
+            self.endpoint = endpoint
+        if not getattr(self, 'data_name', None):
+            self.data_name = data_name
+        if not getattr(self, 'base_url', None):
+            self.base_url = base_url if base_url else 'localhost'
+        if not getattr(self, 'port', None):
+            self.port = port if port else None
+        if self.port:
+            self.base_url = '{}:{}'.format(self.base_url, self.port)
+        if not self.base_url.endswith('/'):
+            self.base_url = self.base_url + '/'
+        self.urls = _get_urls(self.base_url, url_map)
+        self.endpoints = self.urls.keys()
+
+    def _check_response(self, response, *args, **kwargs):
+        """Verify we have got a response without any errors.
+
+        *Never* call this directly in your methods,
+        *Always use self._get() etc, otherwise errors will not
+        be reported correctly.
+        """
+        error = False
+        error_msg = "Unknown error from SEED API"
+        # OK, Created , Accepted
+        if response.status_code not in [200, 201, 202]:
+            error = True
+            error_msg = "SEED returned status code: {}".format(
+                response.status_code
+            )
+        # SEED adds a status key to the response to indicate success/error
+        # This is superfluous as status codes should be used to indicate an
+        # error, but theyt are not always set correctly.
+        elif response.json().get('status', None) == 'error':
+            error = True
+        if error:
+            if response.content:
+                try:
+                    error_msg = response.json().get(
+                        'message', 'Unknown SEED Error'
+                    )
+                except ValueError:
+                    error_msg = 'Unknown SEED Error'
+            if args:
+                kwargs['args'] = args
+            self._raise_error(response, error_msg, stack_pos=1, **kwargs)
+
+    def _get_result(self, response, data_name=None, **kwargs):
+        """Extract result data from response."""
+        if not data_name:
+            url = response.request.url
+            # take the last part of the url unless it's a digit
+            # in which case take the previous part
+            durl = url.lstrip(self.base_url).rstrip('/').rsplit('/', 1)
+            if durl[1].isdigit():
+                data_name = durl[0].rsplit('/', 2)[1]
+            else:
+                data_name = durl[1]
+        # actual results should be under data_name or the fallbacks
+        for dname in [data_name, 'data', 'detail']:
+            try:
+                result = response.json().get(dname)
+                if result is not None:
+                    break
+            except KeyError:
+                pass
+        if result is None:
+            error_msg = "Could not find result using data_name {}.".format(
+                data_name
+            )
+            self._raise_error(response, error_msg, stack_pos=2, **kwargs)
+        return result
+
+    def _raise_error(self, response, error_msg, stack_pos=0, *args, **kwargs):
+        """
+        Raise SEEDError on bad response.
+
+        This method is intended for use only by self_get() etc and the methods
+        called there. For most purposes you should raise SEEDError directly.
+
+        This method uses the inspect module to derive the method name.
+        stack_pos indicates where in the stack to find this: it corresponds
+        to the depth of function calls.
+
+        Thus if the error occurs directly in the function calling _raise_error
+        stack_pos=0, if that function is called by another function add 1 etc.
+        Note techically *this* method (_raise_error) is at the bottom of the
+        stack, but we add 1 to stack_pos so counting starts at the method
+        that calls this one.
+
+
+        :param response: response object
+        :param error_msg: error message
+        :param stack_pos: indicates depth of stack
+        :type stack_pos: integer
+        """
+        status_code = response.status_code
+        url = response.request.url
+        verb = response.request.method
+        # e.g. MyClass.method
+        caller = '{}.{}'.format(
+            self.__class__.__name__, inspect.stack()[stack_pos + 1][3]
+        )
+        if args:
+            kwargs['args'] = args
+        raise SEEDError(
+            error_msg, url=url, caller=caller, verb=verb,
+            status_code=status_code, **kwargs
+        )
+
+    def _set_params(self, params):
+        """Add org_id"""
+        params['org_id'] = self.org_id
+        return params
+
+
+# SEEDClient Mixins, used to add CRUD Methods
+# These should be used with SEEDClient as they rely on its methods
+
+
+class CreateMixin(object):
+    """Add _post methods"""
+    # pylint:disable=too-few-public-methods
+
+    def post(self, endpoint=None, data_name=None, **kwargs):
+        """
+        Post to SEED API (Create record)
+
+        :param endpoint: endpoint name.
+        :param url: url to call
+        :param data_name: key response data is stored under
+
+        :returns: dict (from response.json()[data_name])
+        """
+        kwargs = self._set_params(kwargs)
+        endpoint = _set_default(self, 'endpoint', endpoint)
+        data_name = _set_default(self, 'data_name', data_name, required=False)
+        url = self.urls[endpoint]
+        if not url.endswith('/'):
+            url = url + '/'
+        response = super(CreateMixin, self)._post(url=url, **kwargs)
+        self._check_response(response, **kwargs)
+        return self._get_result(response, data_name=data_name, **kwargs)
+
+
+class ReadMixin(object):
+    """Add get &  list method"""
+    # pylint:disable=too-few-public-methods
+
+    def get(self, pk, endpoint=None, data_name=None, **kwargs):
+        """
+        Get a single record from SEED.
+
+        :param endpoint: endpoint name.
+        :param pk: primary key of record
+        :param data_name: key response data is stored under
+
+        :returns: dict (from response.json()[data_name])
+
+        """
+        kwargs = self._set_params(kwargs)
+        endpoint = _set_default(self, 'endpoint', endpoint)
+        data_name = _set_default(self, 'data_name', data_name, required=False)
+        url = add_pk(self.urls[endpoint], pk, required=True, slash=True)
+        response = super(ReadMixin, self)._get(url=url, **kwargs)
+        self._check_response(response, **kwargs)
+        result = self._get_result(response, data_name=data_name, **kwargs)
+        return result
+
+    def list(self, endpoint=None, data_name=None, **kwargs):
+        """
+        Get all records from SEED.
+
+        :param endpoint: endpoint name.
+        :param data_name: key response data is stored under
+
+        :returns: dict (from response.json()[data_name])
+        """
+        kwargs = self._set_params(kwargs)
+        endpoint = _set_default(self, 'endpoint', endpoint)
+        data_name = _set_default(self, 'data_name', data_name, required=False)
+        url = self.urls[endpoint]
+        if not url.endswith('/'):
+            url = url + '/'
+        response = super(ReadMixin, self)._get(url=url, **kwargs)
+        self._check_response(response, **kwargs)
+        return self._get_result(response, data_name=data_name, **kwargs)
+
+
+class UpdateMixin(object):
+    """Add _put & _patch methods"""
+    # pylint:disable=too-few-public-methods,redefined-builtin
+
+    def put(self, pk, endpoint=None, data_name=None, **kwargs):
+        """
+        Update a record via PUT.
+
+        :param pk: key to put to
+        :param endpoint: endpoint name.
+        :param data_name: key response data is stored under
+
+        :returns: dict (from response.json()[data_name])
+        """
+        kwargs = self._set_params(kwargs)
+        endpoint = _set_default(self, 'endpoint', endpoint)
+        data_name = _set_default(self, 'data_name', data_name, required=False)
+        url = add_pk(self.urls[endpoint], pk, required=True, slash=True)
+        response = super(UpdateMixin, self)._put(url=url, **kwargs)
+        self._check_response(response, **kwargs)
+        return self._get_result(response, data_name=data_name, **kwargs)
+
+    def patch(self, pk, endpoint=None, data_name=None, **kwargs):
+        """
+        Update a record via PATCH.
+
+        :param endpoint: endpoint name.
+        :param pk: key to put to
+        :param data_name: key response data is stored under
+
+        :returns: dict (from response.json()[data_name])
+        """
+        kwargs = self._set_params(kwargs)
+        endpoint = _set_default(self, 'endpoint', endpoint)
+        data_name = _set_default(self, 'data_name', data_name, required=False)
+        url = add_pk(self.urls[endpoint], pk, required=True, slash=True)
+        response = super(UpdateMixin, self)._patch(url=url, **kwargs)
+        self._check_response(response, **kwargs)
+        return self._get_result(response, data_name=data_name, **kwargs)
+
+
+class DeleteMixin(object):
+    """Add _delete methods"""
+    # pylint:disable=too-few-public-methods,redefined-builtin
+
+    def delete(self, pk, endpoint=None, data_name=None, **kwargs):
+        """
+        Delete a record in SEED
+
+        :param endpoint: endpoint name.
+        :param pk: key to put to
+        :param data_name: key response data is stored under
+
+        :returns: None
+        """
+        # pylint:disable=no-member
+        kwargs = self._set_params(kwargs)
+        endpoint = _set_default(self, 'endpoint', endpoint)
+        data_name = _set_default(self, 'data_name', data_name, required=False)
+        url = add_pk(self.urls[endpoint], pk, required=True, slash=True)
+        response = super(DeleteMixin, self)._delete(url=url, **kwargs)
+        # delete should return 204 and no content
+        if response.status_code != requests.codes.no_content:
+            self._check_response(response, **kwargs)
+
+
+class SEEDReadOnlyClient(ReadMixin, SEEDBaseClient):
+    """Read Ony Client"""
+    pass
+
+
+class SEEDReadWriteClient(CreateMixin, ReadMixin, UpdateMixin, DeleteMixin,
+                          SEEDBaseClient):
+    """Client with full CRUD Methods"""
+    # pylint:disable=too-many-ancestors
+    pass
